bd9aaefd298278bd61db4df6f9367efc14304c98
unknown <andante2183@gmail.com>
2018-09-04 19:30:10 +0900

    Modify baseball game

--- star/src/star/BaseBallGame.java
+++ star/src/star/BaseBallGame.java
@@ -9,55 +9,61 @@ public class BaseBallGame {
 		int[] arr2 = new int[3];
 		int sCnt = 0; // 스트라이크 갯수
 		int bCnt = 0; // 볼 갯수
+		int tryCnt = 0; // 시도 횟수
 		
 		// 1. 배열에 숫자 3개 중복없이 채우기
 		
 		int[] numArr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 		
-		for(int i=0;i<numArr.length*5;i++){
-			int n = (int)(Math.random()*8)+1;
-			int tmp = numArr[0];
-			numArr[0] = numArr[n];
-			numArr[n] = tmp;
+		for(int i=0;i<numArr.length;i++){
+			int n = (int)(Math.random()*9)+1;
+			int tmp = numArr[i];
+			numArr[i] = numArr[n-1];
+			numArr[n-1] = tmp;
 		}
 		for(int i=0;i<3;i++){
 			arr1[i] = numArr[i];
 		}
 		
-//		System.out.println(Arrays.toString(arr1));
+//		System.out.println(Arrays.toString(arr1)); 정답부분
 		
 		// 승리할때까지 반복
-		while(sCnt!=3){
+		While1 : while(sCnt!=3){
 			sCnt = 0;
 			bCnt = 0;
 			
 			// 2. 사용자에게 3개의 숫자를 입력받아 arr2 배열에 넣기
+			// 문자를 4개이상 입력받으면 continue; 이용해서 다시 입력받기
 			
 			Scanner scan = new Scanner(System.in);
 			System.out.print("3자리 숫자를 입력하세요");
-			String input = scan.nextLine();
-			int num;
-			num = Integer.parseInt(input);
-		
-//			숫자가 아닌 것을 입력했을 때 오류출력 하고싶었으나 실패
-//			try {
-//				num = Integer.parseInt(input);
-//			} catch (NumberFormatException e) {
-//				// TODO Auto-generated catch block
-//				System.out.print("숫자가 아닙니다. 3자리 숫자를 입력해주세요");
-//				input = scan.nextLine();
-//			}
 			
-			if(!(0 <= num && num <=999)){
-				//범위 초과시 경고문 출력후 재입력
-				System.out.print("범위를 초과하였습니다. 3자리 숫자를 입력해주세요");
-				input = scan.nextLine();
+			// 시도 회수 추가
+			tryCnt++;
+			
+			String input = scan.nextLine();
+			if(input.length()>3){
+				System.out.println("자리수가 초과했습니다.");
+				continue; // while문 처음으로 이동
 			}
-				
+			
+			//System.out.println(input);
+			// "123"
+			// 문자열"123"을 쪼개서 배열에 넣기 -> ['1','2','3']
+			// 1~9까지인 경우, 숫자열 배열로 바꾸기 [1,2,3]
+			// 아닌경우, 다시 입력받기
+			
 			for(int i=0;i<3;i++){
-				arr2[i] = ((int)((num/Math.pow(10, 2-i))%10));
-			} 
-				
+				int tmp = input.charAt(i)-'0';
+				if(1 <=tmp && tmp <=9){
+					arr2[i] = input.charAt(i)-'0';					
+				} else {
+					System.out.println("숫자가 아닙니다.");
+					continue While1; //continue 처음으로 이동
+				}
+			}
+			
+			// 사용자가 입력한 문자열"123"을 배열에 넣어 [1,2,3]으로 보여주기
 			System.out.println(Arrays.toString(arr2));
 					
 			// 3. 두 배열을 비교해서 스트라이크와 볼의 갯수를 계산
@@ -66,7 +72,7 @@ public class BaseBallGame {
 					if(arr1[i]==arr2[j]){
 						// 자리도 같고, 숫자도 같으면
 						if(i==j){
-							sCnt++;	
+							sCnt++;
 						} 
 						// 자리는 다른데, 숫자는 같으면
 						else {
@@ -77,6 +83,6 @@ public class BaseBallGame {
 			}
 			System.out.println(sCnt+"S"+bCnt+"B");
 		}	//end while
-		System.out.println("승리");
+		System.out.println(tryCnt+"번 만에 승리하셨습니다.");
 	} //end main
 }
